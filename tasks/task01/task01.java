b195fc0dee3b78ef32e36f48b26cd6f7293e8745
MuhammetErdur <merdur.35@hotmail.com>
2023-08-18 18:09:14 +0300

    Done

--- tasks/task01/task01.java
+++ tasks/task01/task01.java
@@ -1,12 +1,27 @@
+import org.apache.shiro.authc.*;
+import org.seleniumhq.selenium;
+import
+
+
+
 public class LoginPage {
-    
+
+
+    private AuthenticationService authService;
+
     public boolean login(String username, String password, AuthenticationService authService) {
         boolean result = false;
-        
+        authService instanceof  ? (() authService) : null;
+
+
         // Call the authentication service to check the credentials
         if (authService.authenticate(username, password)) {
+            Assert.assertEqualTo("showErrorMessage")
+
             // If the authentication service returns true, set the result to true
             result = true;
+        } else {
+            AssertassertEqualto("passed")
         }
         
         return result;
